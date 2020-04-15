08e8ce3d974c45c7bfc08688a43fd69edea1dd0c
Laurie Jones <hannahljones98@gmail.com>
2020-04-15 18:28:33 -0400

    cleaned
    got rid of extra variables and print statements

--- Bowling.py
+++ Bowling.py
@@ -1,9 +1,12 @@
 """
-Name: BBowling Game Coding Exercise
+Name: Bowling Game Coding Exercise
 Authors: Laurie Jones
 
 given an integer array of a valid sequence of self.throws for one game of 
 American Ten-Pin Bowling, produces the total score for the game
+
+class: Bowling
+functions: findTotal, spare, strike, next, bonus
 
 """
 
@@ -16,14 +19,7 @@ class Bowling():
         
 
     def findTotal(self):
-        num = 0
         frame = 0
-        first = 0
-        two = 0
-        before = 0
-        after = 0
-        one = 0
-        
 
         for i in range(len(self.throws)):
 
@@ -35,67 +31,48 @@ class Bowling():
                     break
 
             elif self.throws[i] == "/":
-                prev = i - 1
-                first = self.throws[prev]
-                # print("first", first)
-                adder = 10 - int(first)
-                self.total = self.total + adder
-
-                #includes the next value
-                spare = int(self.next(i))
-                self.total = self.total + int(spare)
+                slash = self.spare(i)
+                self.total = self.total + slash
 
             elif (self.throws[i] == "X"):
-
-                self.total = self.total + 10
-                #bc you need to account for the number you are on,
-                #the dash, the next number and then finally you get 
-                #to the number you want
-
-                #this gets the next number
-                one =  int(self.next(i))
-                two = self.next(i+2)
-
-                num = one + int(two)
-                # print("num",num)
-
-                self.total = self.total + int(num)
+                X = self.strike(i)
+                self.total = self.total + X
                 
             else:
-                integer = int(self.throws[i])
-                self.total = self.total + integer
-                #print(self.total)
+                self.total = self.total + int(self.throws[i])
 
-        # print("frames",frame)
-
-        return print(self.total)
-
-    
+        return print("Your total points are:", self.total)
 
 
-    #accepts the position of the number you want to find the next of 
-    def next(self, position):
-        number = 0 
-        first = 0
-        prev = 0
 
-        #skips to next number  
+# accpets: a position integer
+# returns: all of the points aquired in the frame with the spare
+    def spare(self, a):
+        prevNum = self.throws[a-1]
+        adder = 10 - int(prevNum)
+        return adder + int(self.next(a))
+
+
+# accpets: a position integer
+# returns: all of the points aquired in the frame with the spare
+    def strike(self,b):
+        one =  int(self.next(b+1))
+        two = int(self.next(b+2))
+        return one + two + 10
+
+
+#accepts the position of the number you want to find the next of 
+#returns the next number in the game
+    def next(self, position): 
         position = position + 1
 
         if self.throws[position] == '-':
             position = position + 1
 
         if self.throws[position] == "/":
-            prev = position -1
-            # print("prev", prev)
-
-            first = self.throws[position-1]
-            # print("first", first)
-
-            adder = 10 - int(first)
-            number = adder
+            prev = self.throws[position-1]
+            number = 10 - int(prev)
             
-
         elif self.throws[position] == "X":
             number = 10
 
@@ -105,22 +82,19 @@ class Bowling():
         return number
         
 
-
+#accepts the position of the first number in the 10th frame 
+#returns the points you recieve with the 10th frame
     def bonus(self, position):
         
         if self.throws[position] == "X":
-            bFirst = self.next(position)
-            position = position + 1
-            bSecond = self.next(position)
-
-            bPts = int(bFirst) + int(bSecond) + 10
+            bPts = int(self.next(position)) + int(self.next(position + 1)) + 10
 
         elif self.throws[position+1] == "/":
             extra = self.throws[position+2]
 
             if extra == "X":
                 extra = 10
-            
+
             bPts = 10 + int(extra)
 
         else:
@@ -132,33 +106,37 @@ class Bowling():
 
 
 def main():
-    #throws = input("Enter your throws for your game: ")
-
-    print("it is done!")
-    print("---- testing spares -------")
-    throws = "5/-5/-5/-5/-5/-5/-5/-5/-5/-5/5"
+#run it yourself
+    throws = input("Enter your throws for your game: ")
     b = Bowling(throws)
     b.findTotal()
-    print("should be 150")
-
-    print("---- testing integers -------")
-    throws = "45-54-36-27-09-63-81-18-90-72"
-    b = Bowling(throws)
-    b.findTotal()
-    print("should be 90")
 
 
-    print("---- testing strikes -------")
-    throws = "X-X-X-X-X-X-X-X-X-XXX"
-    b = Bowling(throws)
-    b.findTotal()
-    print("should be 300")
+#tests
+    # print("---- testing spares -------")
+    # throws = "5/-5/-5/-5/-5/-5/-5/-5/-5/-5/5"
+    # b = Bowling(throws)
+    # b.findTotal()
+    # print("should be 150")
 
-    print("---- testing fun cases -------")
-    throws = "X-X-5/-44-X-72-7/-X-81-9/X"
-    b = Bowling(throws)
-    b.findTotal()
-    print("should be 163")
+    # print("---- testing integers -------")
+    # throws = "45-54-36-27-09-63-81-18-90-72"
+    # b = Bowling(throws)
+    # b.findTotal()
+    # print("should be 90")
+
+
+    # print("---- testing strikes -------")
+    # throws = "X-X-X-X-X-X-X-X-X-XXX"
+    # b = Bowling(throws)
+    # b.findTotal()
+    # print("should be 300")
+
+    # print("---- testing fun cases -------")
+    # throws = "X-X-5/-44-X-72-7/-X-81-9/X"
+    # b = Bowling(throws)
+    # b.findTotal()
+    # print("should be 163")
 
 if __name__ == "__main__":
    main()
