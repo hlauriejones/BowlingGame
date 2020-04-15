2a84a36a76d44f4483e4728e431317333cb90761
Laurie Jones <hannahljones98@gmail.com>
2020-04-15 15:47:46 -0400

    spare is fixed
    misread the instructions but now if you throw a spare it includes the next throw

--- Bowling.py
+++ Bowling.py
@@ -15,58 +15,92 @@ class Bowling():
         self.total = 0
         self.ptr = iter(self.throws)
         
-    #iterate through the input
+
     def findTotal(self):
         num = 0
-
 
         for i in range(len(self.throws)):
 
             if self.throws[i] == "-":
                 self.total = self.total + 0
 
-        #if its a / 
-        #either subtract the last number and add 10
-            elif self.throws[i] == "/": 
-                self.total = self.total + 10
+            elif self.throws[i] == "/":
 
+                #adds up to 10
+                prev = i - 1
+                first = self.throws[prev]
+                print("first", first)
+                adder = 10 - int(first)
+                self.total = self.total + adder
 
-        #if its a X then add 10 to the total
-        #plus the total pins knocked down on the next two self.throws.
+                #includes the next value
+                spare = int(self.next(i))
+                self.total = self.total + int(spare)
+
             elif self.throws[i] == "X":
                 self.total = self.total + 10
-                num = int(self.ex(i+1)) + int(self.ex(i+2))
+                num = int(self.next(i)) #+ int(self.ex(i+2))
                 print("num",num)
                 self.total = self.total + int(num)
                 
-                
-        #add up the numbers  
             else:
                 integer = int(self.throws[i])
                 self.total = self.total + integer
                 #print(self.total)
 
-            #print("j",j)
             print("number", self.throws[i])
             print("runningtotal",self.total)
 
         return print(self.total)
 
 
+    #accepts the position of X 
+    def next(self, position):
+        number = 0 
 
-    #return the value of the next function
-    def ex(self, position):
-        print("pos", position)
-        position = position +1
+        #skips to next number  
+        position = position + 1
 
-        if self.throws[position] == "/": 
-            return 10
+        if self.throws[position] == "-":
+            position = position + 1
 
-        elif self.throws[position] == "X":
-            return 10 + int(self.ex(position + 1))
+        if self.throws[position] == "/":
+            prev = position -1
+
+            first = self.throws[prev]
+            print("first", first)
+
+
+            adder = 10 - first
+            number = adder
+
+
+        if self.throws[position] == "X":
+            number = 10
+
+        else: 
+            number = self.throws[position]
+
+        return number
         
-        else:
-            return self.throws[position]
+
+
+
+    # def spare(self, position):
+    #     print("spare")
+
+    #     if self.throws[position] == "-":
+    #         position = position +1
+
+    #     if self.throws[position] == "X":
+    #         return 10 + int(self.ex(position + 1))
+        
+    #     else: 
+    #         return self.throws[position+1]
+
+
+
+
             
         
            
@@ -75,10 +109,10 @@ class Bowling():
 
 def main():
     #throws = input("Enter your throws for your game: ")
-    throws = "23-5/-X-X-44"
+    throws = "23-5/-44"
     b = Bowling(throws)
     b.findTotal()
-    print("should be 46")
+    print("should be 27 ")
 
 
 if __name__ == "__main__":
